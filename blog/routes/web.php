ab912bc62050f670e3d8079e1bb6165fa70be844
grijalvaitsncg <89266451+grijalvaitsncg@users.noreply.github.com>
2025-08-27 15:26:21 -0700

    Modificacion a la BD
    Creación de la tabla posts

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 //ENDPOINT
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("/contacto",function(){
+Route::get("/contact",function(){
     return view('contacto');
 });
+Route::get("/post",function(){
+    return view('post');
+});
+
+Route::get("/about",function(){
+    return view('about');
+});
+
